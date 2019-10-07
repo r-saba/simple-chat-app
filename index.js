92aee9b28c3bcf1febb619abfc900ce3a0f6b3f9
ragith <ragith.coolkid@gmail.com>
2019-10-06 20:34:39 -0400

    Add nick name support

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ app.get('/', function(req, res){
 })
 
 io.on('connection', function(socket){
+	socket.name = 'Anonymous';
+	socket.broadcast.emit('chat message','user connected');
 	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
+		if(msg.includes('/name')){
+			let nickName = msg.substring(5).trim();
+			socket.name = nickName
+			console.log(socket.name);
+		} else {
+			let chatMessage = socket.name + ': ' + msg;
+			io.emit('chat message', chatMessage);
+		}
 	});
 	socket.on('disconnect', function(){
-		console.log('user disconnected');
+		socket.broadcast.emit('chat message','user disconnected');
 	})
 })
 
